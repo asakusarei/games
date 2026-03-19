591fe3e1b06f96041b1c924417263d1af3c23015
taiyakipurin <167001319+taiyakipurin@users.noreply.github.com>
2026-03-20 01:15:35 +0300

    feat: add timer and score
    - add food spawn
    - add simple timer
    - add score counter

--- snake.c
+++ snake.c
@@ -2,24 +2,57 @@
 #include <conio.h>
 #include <stdlib.h>
 #include <windows.h>
+#include <time.h>
 
 #define X 20
 #define Y 10
+#define FOOD_SPAWN_DELAY 20
+
+int score;
+int time_elapsed;
+bool food_status = true;
 
 void clear_screen()
 {
-	printf("\n\033[H\033[2J");
+	printf("\n\033[H\033[J");
 }
 
-void draw_field(int x, int y)
+int *spawn_food()
+{
+	int *food_coordinates = malloc(sizeof(int) * 2);
+	int x_pos, y_pos;
+	
+	do
+	{
+		x_pos = rand() % 10;
+		y_pos = rand() % 10;
+	}
+	while (x_pos > X-2 || x_pos == 0 || y_pos > Y-2 || y_pos == 0);
+	
+	food_coordinates[0] = x_pos;
+	food_coordinates[1] = y_pos;
+	
+	return food_coordinates;
+}
+
+void draw_field(int x, int y, int food_x, int food_y)
 {
 	for (int i = 0; i < Y; i++)
 	{
 		for (int j = 0; j < X; j++)
-		{
+		{	
 			if (j == x && i == y)
 			{
 				printf("o");
+				if (j == x && j == food_x && i == y && i == food_y)
+				{
+					score++;
+					food_status = false;
+				}
+			}
+			else if (j == food_x && i == food_y && food_status == true)
+			{
+					printf("*");
 			}
 			else if (i == 0 || i == Y - 1 || j == 0 || j == X - 1)
 			{
@@ -34,39 +67,66 @@ void draw_field(int x, int y)
 	}
 }
 
+int stat_screen(struct timespec start, struct timespec current)
+{
+	double time_elapsed = 
+		(current.tv_sec - start.tv_sec) + 
+		(current.tv_nsec - start.tv_nsec) / 1e9;
+	
+	printf("\ntime elapsed: %.2f\n", time_elapsed);
+	printf("score: %d\n", score);
+	
+}
+
 int main(void)
 {
+	srand(time(NULL));
+	
 	int x = X/2;
 	int y = Y/2;
-	
 	int dx = 0;
 	int dy = 0;
 	
+	int *food_coordinates = spawn_food();
+	int food_x = food_coordinates[0];
+	int food_y = food_coordinates[1];
+	int iter_count = 0;
+	
+	struct timespec start, current;
+	clock_gettime(CLOCK_MONOTONIC, &start);
 	
 	while (1)
 	{
 		if (_kbhit())
 		{
 			char ch = _getch();
-			if (ch == 'w')
+
+			switch (ch)
 			{
-				dx = 0;
-				dy = -1;
-			}
-			else if (ch == 'a')
-			{
-				dx = -1;
-				dy = 0;
-			}
-			else if (ch == 'd')
-			{
-				dx = 1;
-				dy = 0;
-			}
-			else if (ch == 's')
-			{
-				dx = 0;
-				dy = 1;
+				case 'w':
+				{
+					dx = 0;
+					dy = -1;
+					break;
+				}
+				case 'a':
+				{
+					dx = -1;
+					dy = 0;
+					break;
+				}
+				case 'd':
+				{
+					dx = 1;
+					dy = 0;
+					break;
+				}
+				case 's':
+				{
+					dx = 0;
+					dy = 1;
+					break;
+				}
 			}
 		}
 		
@@ -89,13 +149,28 @@ int main(void)
 		{
 			y = Y-2;
 		}
-		
+
 		clear_screen();
-		draw_field(x, y);
+		
+		if (iter_count > FOOD_SPAWN_DELAY)
+		{
+			free(food_coordinates);
+			int *food_coordinates = spawn_food();
+			food_x = food_coordinates[0];
+			food_y = food_coordinates[1];
+			food_status = true;
+			iter_count = 0;
+		}	
+		
+		iter_count++;
+		
+		draw_field(x, y, food_coordinates[0], food_coordinates[1]);
+		
+		clock_gettime(CLOCK_MONOTONIC, &current);
+		stat_screen(start, current);
 	
 		Sleep(300);
 	}
-	
 	
 	return 0;
 }
